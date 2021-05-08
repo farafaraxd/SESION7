c0925079399aee24c1bd5b2a3a9a1e52c37a7dd9
farafara <>
2021-05-08 16:44:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("hola")
+
+
+let imagen=document.getElementById("imagen")
+imagen.style.border="7px solid red"
+
+imagen.style.width="300px"
+
+imagen.style.marginBottom="5px"
+
+let mensaje=document.getElementById("mensaje")
+
+mensaje.style.background="linear-gradient(45deg,#20d6d6,#4e20d6)"
+
+mensaje.style.fontSize="35px"
+
+mensaje.innerText="me gusta el color rojo"
+
+
+
+
+
+
+
